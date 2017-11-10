b5021b8c71837fa31cf360b34dd3cc3a5b989d59
Git Automerge <jcguarinpenaranda@gmail.com>
2017-11-10 16:34:33 -0500

    changing files

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-export default () => (
-  <div>Hello world!</div>
-);
+const propTypes = {
+  // eslint-disable-next-line
+  mapProps: PropTypes.string.object,
+  mapRef: PropTypes.func,
+};
+
+const defaultProps = {
+  mapRef: () => {},
+};
+
+export default class ParseQueryMap extends React.Component {
+  render() {
+    return (
+      /* withScriptjs(withGoogleMap(
+        <GoogleMap
+        {...this.props.mapProps}  
+        />
+      ))*/
+      <div>Hello world</div>
+    );
+  }
+}
+
+ParseQueryMap.propTypes = propTypes;
+ParseQueryMap.defaultProps = defaultProps;
